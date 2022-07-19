597dc72b6ef1a3ccf4af29dd3b96f2a249bbdc3d
Bianca <46465745+biancab8@users.noreply.github.com>
2022-07-19 00:34:54 -0600

    make code work with multiple ghosts

--- Interactive Ghost/app.js
+++ Interactive Ghost/app.js
@@ -1,22 +1,26 @@
 window.addEventListener("mousemove", function(event) {
   // get elements
-  const leftEye = document.querySelector(".eye-left");
-  const rightEye = document.querySelector(".eye-right");
-  const containerDims = document.querySelector(".eye-container").getBoundingClientRect();
-  const eyeDims = leftEye.getBoundingClientRect();
+  const leftEyes = document.querySelectorAll(".eye-left");
+  const rightEyes = document.querySelectorAll(".eye-right");
+  const containerDims = document.querySelectorAll(".eye-container");
+  
+  //for each ghost, move eyes
+  for(let i=0; i<leftEyes.length; i++){
+    const containerDim = containerDims[i].getBoundingClientRect();
+    const eyeDim = leftEyes[i].getBoundingClientRect();
 
-  // get distance between mouse location and center of eyes
-  let xOffset = containerDims.left + (containerDims.width / 2)  + window.scrollX;
-  let yOffset = containerDims.top + (eyeDims.width / 2) + window.scrollY;
-  let xDistance = event.pageX - xOffset;
-  let yDistance = event.pageY - yOffset;
+    // get distance between mouse location and center of eyes
+    let xOffset = containerDim.left + (containerDim.width / 2)  + window.scrollX;
+    let yOffset = containerDim.top + (eyeDim.width / 2) + window.scrollY;
+    let xDistance = event.pageX - xOffset;
+    let yDistance = event.pageY - yOffset;
 
-  // calculate angle for eye rotation
-  let radian = Math.atan2(xDistance, yDistance);
-  let degrees = radian * (180 / Math.PI) * -1 + 180;
+    // calculate angle for eye rotation
+    let radian = Math.atan2(xDistance, yDistance);
+    let degrees = radian * (180 / Math.PI) * -1 + 180;
 
-  // rotate eyes
-  leftEye.style.transform = "rotate(" + degrees + "deg)";
-  rightEye.style.transform = "rotate(" + degrees + "deg)";
-
+    // rotate eyes
+    leftEyes[i].style.transform = "rotate(" + degrees + "deg)";
+    rightEyes[i].style.transform = "rotate(" + degrees + "deg)";
+  }
 });
